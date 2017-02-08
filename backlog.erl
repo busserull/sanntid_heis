45632e59930ac3b4669a21131094d79607acf1d8
Duvarahan Kirishnathasan <duva_92@msn.com>
2017-02-08 11:21:56 +0100

    Backlog experiment 1.1
    Signed-off-by: Duvarahan Kirishnathasan <duva_92@msn.com>

--- backlog.erl
+++ backlog.erl
@@ -14,24 +14,41 @@ completeOrder(From, Pid, Order) ->
 runBacklog(Backlog) ->
     receive
         {From, {store, Order}} ->
-            case lists:member(Order, Backlog) of
-                true ->
-                    From ! ok;
-                false ->
-                    From ! ok,
-                    runBacklog([Backlog|Order])
-            end;
+            From ! ok,
+            runBacklog([Backlog|[Order]]);
+            %case lists:member(Order, Backlog) of
+            %    true ->
+            %        From ! ok,
+            %        runBacklog(Backlog);
+            %    false ->
+            %        From ! ok,
+            %        runBacklog([Backlog|[Order]])
+            %end;
         {From, {complete, Order}} ->
             From ! ok,
             runBacklog(lists:delete(Order, Backlog));
         {From, {update, Order}} ->
             OldOrder = #order{type = Order#order.type, floor = Order#order.floor},
             From ! ok,
-            runBacklog([lists:delete(OldOrder, Backlog)|Order]);
+            runBacklog([lists:delete(OldOrder, Backlog)|[Order]]);
+        debug ->
+            debugPrint(Backlog),
+            runBacklog(Backlog);
         {From, _} ->
-            From ! unrecognized_msg
+            From ! unrecognized_msg,
+            runBacklog(Backlog)
     end.
 
 %% This will read the local file later
 init() ->
     runBacklog([]).
+
+debugPrint([]) ->
+    io:format("~n");
+debugPrint([A|Rest]) ->
+    {order, Type, Floor, State} = A,
+    io:format("~p, ~p, ~p~n", [Type, Floor, State]),
+    debugPrint(Rest).
+
+debug(Pid) ->
+    Pid ! debug.
