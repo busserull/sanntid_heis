9d3f3e667c16e6deb4ca35a461a1d4844bff751f
Austreng <kolbjora@stud.ntnu.no>
2017-02-22 16:49:40 +0100

    Implemented v1 order timeout
    Signed-off-by: Austreng <kolbjora@stud.ntnu.no>

--- bl3.erl
+++ bl3.erl
@@ -3,6 +3,8 @@
 
 % Names an ETS table
 -define(ORTAB, ordertable).
+% Time before an order times out in seconds
+-define(ORTOUT, 5).
 
 -export([start/0, store_order/2, list_orders/0]).
 -export([init/1, handle_call/3, handle_cast/2,
@@ -22,6 +24,7 @@ list_orders() ->
 %%% Server callbacks
 
 init([]) ->
+	erlang:send_after(1000, self(), {timer}),
 	ets:new(?ORTAB, [set, named_table]),
 	State = 0,
 	{ok, State}.
@@ -36,10 +39,19 @@ handle_call({list}, _From, State) ->
 
 handle_cast(_Msg, State) ->
 	{noreply, State}.
+
+handle_info({timer}, State) ->
+	erlang:send_after(1000, self(), {timer}),
+	% Check all external orders for timeout
+	check_for_timeout(ets:match(?ORTAB, {{ext,'$1'},'_',queued,'$2','_'})),
+	%check_for_timeout(ets:first(?ORTAB)),
+	{noreply, State};
 handle_info(_Msg, State) ->
 	{noreply, State}.
+
 terminate(_Reason, _State) ->
 	ok.
+
 code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
 
@@ -51,3 +63,15 @@ list_orders(Order) ->
 	io:format("~p~n", [ets:lookup(?ORTAB, Order)]),
 	list_orders(ets:next(?ORTAB, Order)).
 
+check_for_timeout([]) ->
+	ok;
+check_for_timeout([[Floor|[Timestamp]]|Tail]) ->
+	Now = erlang:monotonic_time(),
+	Elapsed = erlang:convert_time_unit(Now - Timestamp, native, second),
+	if Elapsed >= ?ORTOUT ->
+		   io:format("{~p, ~p} timed out!~n", [ext, Floor]),
+		   ets:update_element(?ORTAB, {ext, Floor}, {3, timeout});
+	   true ->
+		   ok
+	end,
+	check_for_timeout(Tail).
