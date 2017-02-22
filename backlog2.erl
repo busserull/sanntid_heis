536f0e71bb9b5042dfc6f61dd1587fb172e14c49
Austreng <kolbjora@stud.ntnu.no>
2017-02-22 11:22:37 +0100

    ETS has to be created 'clientside'
    Signed-off-by: Austreng <kolbjora@stud.ntnu.no>

--- backlog2.erl
+++ backlog2.erl
@@ -5,33 +5,44 @@
 -define(STORETIMEOUT, 500).
 
 % Interface
--export([start/1, stop/0, store_order/2]).
+-export([start_link/0, stop/0, store_order/2, list_order/0]).
 % Server callbacks
 -export([init/1, handle_call/3, handle_cast/2,
 		handle_info/2, terminate/2, code_change/3]).
 
-start(Args) ->
-	io:format("Starting server with ~p~n", [Args]),
-	gen_server:start_link({local, ?MODULE}, ?MODULE, [Args], []).
+start_link() ->
+	io:format("Called start_link()~n"),
+	gen_server:start_link(?MODULE, [], []).
 
 stop() ->
 	io:format("Shutting server down~n"),
-	gen_server:cast(?MODULE, shutdown).
+	gen_server:cast(?MODULE, terminate).
 
 store_order(Type, Floor) ->
 	Order = {{Type, Floor}, {127,0,0,1}, queued, erlang:monotonic_time(), 0},
-	gen_server:call(?MODULE, {store, Order}, ?STORETIMEOUT).
+	ets:insert(?ORDERDB, Order).
+	%gen_server:call(?MODULE, {store, Order}, ?STORETIMEOUT).
+
+list_order() ->
+	list_order(ets:first(?ORDERDB)).
+list_order(Order) ->
+	case Order of
+		'$end_of_table' ->
+			{ok};
+		_ ->
+			io:format("~p~n", [Order]),
+			list_order(ets:next(?ORDERDB, Order))
+	end.
 
 %% ===================
 %% Server callbacks
 %% ===================
-init([Args]) ->
-	io:format("Initializing server with ~p~n", [Args]),
-	ets:new(?ORDERDB, [set, protected, named_table, {keypos, 5},
+init([]) ->
+	io:format("Called init()~n"),
+	ets:new(?ORDERDB, [set, protected, named_table, {keypos, 1},
 						 {heir, none}, {read_concurrency, false},
 						 {write_concurrency, false}]),
-	%process_flag(trap_exit, true),
-	{ok, initialized}.
+	{ok, ?ORDERDB}.
 
 handle_call(Msg, From, State) ->
 	io:format("Received message [~p, ~p, ~p]~n", [Msg, From, State]),
