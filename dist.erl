7243a076a19643f0313274fae7d282b6af12f10e
Austreng <kolbjora@stud.ntnu.no>
2017-03-02 12:12:51 +0100

    Begun work on UDP Peer Discovery
    Signed-off-by: Austreng <kolbjora@stud.ntnu.no>

--- dist.erl
+++ dist.erl
@@ -3,12 +3,15 @@
 
 -define(ELEVNAME, "beastly_beast@").
 -define(COOKIE, 'beistheis').
+-define(PEER_DISC_PORT, 23600).
+-define(BROADCAST_TIME, 15000).
 
 start() ->
-	net_kernel:start([get_ip(), longnames]),
-	erlang:set_cookie(node(), ?COOKIE).
+	net_kernel:start([get_hostname(), longnames]),
+	erlang:set_cookie(node(), ?COOKIE),
+	{ok, Socket} = gen_udp:open(?PEER_DISC_PORT, [{broadcast, true}, binary]).
 
-get_ip() ->
+get_hostname() ->
 	{ok, [IPTuple | _]} = inet:getif(),
 	OurIP = tuple_to_list(element(1, IPTuple)),
 	[_ | Tail] = lists:flatmap(fun(Seg) -> [".", Seg] end, OurIP),
