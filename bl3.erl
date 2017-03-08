b8ebcfc469b8e1bfd337200fb559d7f31dd5a4a5
Kolbjørn Austreng <kolbjora@stud.ntnu.no>
2017-03-08 16:05:39 +0100

    Started distributed backlog design
    Signed-off-by: Kolbjørn Austreng <kolbjora@stud.ntnu.no>

--- bl3.erl
+++ bl3.erl
@@ -6,7 +6,7 @@
 % Time before an order times out in seconds
 -define(ORTOUT, 5).
 
--export([start/0, store_order/2, claim_order/3, clear_order/2, list_orders/0]).
+-export([start/0, store_order/2, claim_order/2, clear_order/2, list_orders/0]).
 -export([init/1, handle_call/3, handle_cast/2,
 		handle_info/2, terminate/2, code_change/3]).
 
@@ -87,11 +87,13 @@ order_dir_penalty(_ElevDir, _OrderFloor, _OrderDir) -> 0.
 start() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+-spec store_order(up|down|int, Floor :: integer()) -> ok.
 store_order(Type, Floor) ->
 	gen_server:call(?MODULE, {store, {Type, Floor}}).
 
-claim_order(Type, Floor, IP) ->
-	gen_server:call(?MODULE, {claim, {Type, Floor, IP}}).
+-spec claim_order(up|down|int, Floor :: integer()) -> ok.
+claim_order(Type, Floor) ->
+	gen_server:call(?MODULE, {claim, {Type, Floor}}).
 
 clear_order(Type, Floor) ->
 	gen_server:call(?MODULE, {clear, {Type, Floor}}).
@@ -107,14 +109,31 @@ init([]) ->
 	State = 0,
 	{ok, State}.
 
+% Store order
 handle_call({store, {Type, Floor}}, _From, State) ->
-	Order = {{Type, Floor}, {127,0,0,1}, queued, erlang:monotonic_time()},
+	Key = case Type of
+		   int ->
+			{int, node()};
+		   _ ->
+		   	{ext, Type}
+	end,
+	Order = {{Key, Floor}, queued, erlang:monotonic_time()},
 	ets:insert(?ORTAB, Order),
+	rpc:multicall(nodes(), ets, insert, [?ORTAB, {12,34,56}]),
 	{reply, ok, (State + 1)};
+% Claim order
+%handle_call({claim, {int, Floor}}, _From, State) ->
+%	ok;
+%	% Just update the order everywhere
+%handle_call({claim, {Type, Floor}}, _From, State) ->
+%	ok;
+	
+
 handle_call({claim, {Type, Floor, IP}}, _From, State) ->
 	Now = erlang:monotonic_time(),
 	ets:update_element(?ORTAB,{Type, Floor},[{2,IP},{3,claimed},{4,Now}]),
 	{reply, ok, State};
+
 handle_call({clear, {Type, Floor}}, _From, State) ->
 	Types = case Type of
 		internal ->
@@ -136,7 +155,8 @@ handle_cast(_Msg, State) ->
 handle_info({timer}, State) ->
 	erlang:send_after(1000, self(), {timer}),
 	% Check all external orders for timeout
-	check_for_timeout(ets:match(?ORTAB, {{ext,'$1'},'_',claimed,'$2'})),
+	check_for_timeout(ets:match(?ORTAB, {{up,'$1'},'_',claimed,'$2'})),
+	check_for_timeout(ets:match(?ORTAB, {{down,'$1'},'_',claimed,'$2'})),
 	%check_for_timeout(ets:first(?ORTAB)),
 	{noreply, State};
 handle_info(_Msg, State) ->
