051301bf545e40b27d7bb73006ac3c3865745b9f
AndreasVaage <student@itk-d650>
2017-03-13 21:27:59 +0100

    Started implementing sort_on_distance
    Signed-off-by: AndreasVaage <student@itk-d650>

--- cost.erl
+++ cost.erl
@@ -9,7 +9,7 @@
 -define(DIFF_DIR_PENALTY, 1).
 -define(INITIAL_COST, 500).
 
--export([optimal/3, get_cost/3]).
+-export([optimal/3, get_cost/3, sort_on_distance/2]).
 
 optimal(ElevFloor, ElevDir, Key) ->
     optimal(ElevFloor, ElevDir, Key, ?INITIAL_COST, none).
@@ -54,3 +54,35 @@ get_cost(ElevFloor, ElevDir, Key) ->
                 end,
     TotalCost = TypePen + DirPen + FloorPen + StatusPen,
     TotalCost.
+
+sort_on_distance(List, {Floor, Dir, Pos}) ->
+    Sort_fun = 
+    fun({{int, _Node}, Floor}, {_State, _Floor}) when Pos =:= at_floor ->
+        true;
+        ({{ext, Dir}, Floor}, {_State, _Floor}) when Pos =:= at_floor ->
+        true;
+        ({{int, _Node}, FloorA}, {{int, _Node}, FloorB}) ->
+            case dir of
+              up when FloorA > Floor, FloorB > Floor ->
+                FloorA =< FloorB;
+              up when FloorA > Floor ->
+                true;
+              up when FloorB > Floor ->
+                false;
+              up ->
+                FloorA > FloorB;
+              down when FloorA < Floor, FloorB < Floor ->
+                FloorA >= FloorB;
+              down when FloorA < Floor ->
+                true;
+              down when FloorB < Floor ->
+                false;
+              down ->
+                FloorA < FloorB
+            end;
+        ({{ext, up}, FloorA}, {{ext, up}, FloorB}) when FloorA > Floor, FloorB > Floor ->
+        FloorA =< FloorB;
+        ({{ext, up}, FloorA}, {{ext, up}, FloorB}) when FloorA > Floor, FloorB > Floor ->
+        FloorA =< FloorB
+    end,
+    lists:sort(Sort_fun, List).
