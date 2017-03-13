ff61d3d5659b8e22d458a7a73bc3f469d8b39710
Kolbjørn Austreng <kolbjora@stud.ntnu.no>
2017-03-13 23:27:45 +0100

    Simplifying interface
    Signed-off-by: Kolbjørn Austreng <kolbjora@stud.ntnu.no>

--- backlog.erl
+++ backlog.erl
@@ -7,7 +7,7 @@
 
 %%% API
 -export([start/0, store_order/2, alter_order/3, notify_state/3,
-         assign_order/1, get_order/0, list/0]).
+         get_order/0, list/0]).
 %%% Helper functions
 -export([sync_orders/0, helper_sync/0, make_order_key/1]).
 %%% Server callback functions
@@ -29,18 +29,9 @@ store_order(Type, Floor) ->
     Key = make_order_key(Type),
 	Order = {{Key, Floor}, queued, erlang:monotonic_time()},
     rpc:multicall(gen_server, call, [?MODULE, {store, Order}]).
-    %case lists:keyfind(size,1,ets:info(?ORTAB)) of
-    %	0 ->
-    %		assign_order(Order);
-    %	_Size ->
-    %		ok
-    %end.
 
 notify_state(ElevFloor, ElevDir, AtFloor) ->
     gen_server:call(?MODULE, {notify, {ElevFloor, ElevDir, AtFloor}}).
-
-
-
 
 alter_order(Type, Floor, NewState) ->
     Key = {make_order_key(Type), Floor},
@@ -48,28 +39,6 @@ alter_order(Type, Floor, NewState) ->
 
 get_order() ->
     gen_server:call(?MODULE, get_order).
-%    {{Type, Dir}, Floor} = cost:optimal(ElevFloor, ElevDir, ets:first(?ORTAB)),
-%    Key = case Type of
-%              ext ->
-%                  {Dir, Floor};
-%              int ->
-%                  {int, Floor}
-%          end,
-%    io:format("Best fit: ~p~n", [Key]).
-
-assign_order(Key) ->
-    {Costs, _} = rpc:multicall(gen_server, call, [?MODULE, {get_cost, Key}]),
-    {_Cost, _Node} = lists:min(Costs),
-    %io:format("Before rpc~n"),
-    %rpc:call([Node()], gen_server, cast, [?MODULE, {assign, Key}]),
-    %io:format("After rpc~n"),
-    gen_server:cast(?MODULE, {assign, Key}),
-    {{Type, _Dir}, Floor} = Key,
-    rpc:multicall(?MODULE, alter_order, [Type, Floor, claimed]).
-
-
-%%%%%%%%%
-
 
 %%% Server callbacks
 
@@ -77,7 +46,7 @@ assign_order(Key) ->
 init([]) ->
 	ets:new(?ORTAB, [set, named_table]),
     erlang:send_after(?TOUTCHINT, self(), timer),
-	{ok, {{0, stop, at_floor}, []}}.
+	{ok, {{0, stop, at_floor}, none}}.
     % Likely incorrect inital state, this only
     % impacts the first time get_cost is called
 
@@ -85,7 +54,8 @@ init([]) ->
 handle_call({store, Order}, _From, State) ->
     ets:insert(?ORTAB, Order),
     {Key, _Status, _Timestamp} = Order,
-    set_button_light(Key, on),
+    Key,
+    %set_button_light(Key, on), %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -> X
     {reply, ok, State};
 
 % Alter order
@@ -108,46 +78,42 @@ handle_call({alter, Key, NewState}, _From, State) ->
     ets:update_element(?ORTAB, Key, [{2, NewState}, {3, Now}]),
     {reply, ok, State};
 
-% Get order
-handle_call(get_order, _From, {State, OldList}) ->
-    io:format("get_order: (~p)~n", [State]),
-    OrderList = OldList,
-    {ElevFloor, _Dir, _AtFloor} = State,
-    Diff = case OrderList of
-                  [] ->
-                      none;
-                  _ ->
-                      element(2, hd(OrderList)) - ElevFloor
+% Get order %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Updates State
+handle_call(get_order, _From, {State, OldOrder}) ->
+    {ElevFloor, Dir, AtFloor} = State,
+    % If none, attempt to get some, then update global backlog
+    CurrentOrder = case OldOrder of
+                       none when AtFloor == at_floor ->
+                           cost:optimal(ElevFloor, Dir, ets:first(?ORTAB));
+                       none when AtFloor == in_the_void andalso Dir == up ->
+                           cost:optimal(ElevFloor + 1, Dir, ets:first(?ORTAB));
+                       none when AtFloor == in_the_void andalso Dir == down ->
+                           cost:optiamL(ElevFloor - 1, Dir, ets:first(?ORTAB));
+                       _ ->
+                           OldOrder
+                   end,
+    %
+    Diff = case CurrentOrder of
+               none ->
+                   none;
+               _ ->
+                   element(2, CurrentOrder) - ElevFloor
            end,
-    Command = case Diff of
-                  none ->
-                      none;
-                  Num when Num == 0 ->
-                      open_door;
-                  Num when Num > 0 ->
-                      up;
-                  Num when Num < 0 ->
-                      down
-              end,
-    io:format("Returning ~p~n", [Command]),
-    {reply, Command, {State, OrderList}};
+    {NewOrder, Command} = case Diff of
+                              none ->
+                                  {none, none};
+                              Num when Num == 0 ->
+                                  {none, open_door};
+                              Num when Num > 0 ->
+                                  {CurrentOrder, up};
+                              Num when Num < 0 ->
+                                  {CurrentOrder, down}
+                          end,
+    {reply, Command, {State, NewOrder}};
 
-% Update known elevator state
+% Update known elevator state %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Updates State
 handle_call({notify, NewState}, _From, {_OldState, OrderList}) ->
     {reply, ok, {NewState, OrderList}};
-
-% Calculate cost for particular order
-handle_call({get_cost, Key}, _From, {State, OrderList}) ->
-    {Floor, Dir, AtFloor} = State,
-    Cost = case AtFloor of
-               in_the_void when Dir == up ->
-                   cost:get_cost(Floor + 1, Dir, Key);
-               in_the_void when Dir == down ->
-                   cost:get_cost(Floor - 1, Dir, Key);
-               at_floor ->
-                   cost:get_cost(Floor, Dir, Key)
-           end,
-    {reply, {Cost, node()}, {State, OrderList}};
 
 % List orders
 handle_call({list}, _From, State) ->
@@ -167,19 +133,6 @@ handle_info(timer, State) ->
 
 handle_info(_Msg, State) ->
 	{noreply, State}.
-
-% Be assigned order
-handle_cast({assign, Key}, {State, OldList}) ->
-    NewList = case lists:member(Key, OldList) of
-                  true ->
-                      OldList;
-                  %false when length(OldList) == 0 ->
-                  %    environment_controller:goto_order(Key),
-                  %    OldList ++ [Key];
-                  false ->
-                  	OldList ++ [Key]
-              end,
-    {noreply, {State, NewList}};
 
 handle_cast(_Msg, State) -> {noreply, State}.
 
